99ca9d9881d58cefcf0b598374b4facd99c30486
Constantine Lignos <lignos@seas.upenn.edu>
2011-09-07 17:04:07 -0400

    [c$] Added copyright messages.

--- SubSimNLDemo/Demo.cs
+++ SubSimNLDemo/Demo.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+ *  Demo.cs - Launches the NL SubSim for standalone testing.
+ *  Copyright (C) 2011 Constantine Lignos
+ *
+ *  This file is part of SumSimNL.
+ *
+ *  SumSimNL is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SumSimNL is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SumSimNL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Windows.Forms;
 using Brain;
 using SubSimProcessorLanguage;
--- SubSimNLProcessor/semantics_interface.py
+++ SubSimNLProcessor/semantics_interface.py
@@ -1,3 +1,22 @@
+
+#  semantics_interface.py - Provides an interface to semantics functions.
+#  Copyright (C) 2011 Constantine Lignos
+#
+#  This file is part of SumSimNL.
+#
+#  SumSimNL is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SumSimNL is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SumSimNL.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 
 from Semantics import Knowledge, Parsing, Tree
