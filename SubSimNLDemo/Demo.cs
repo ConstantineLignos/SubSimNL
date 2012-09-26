67ef58fa4cefa3594e8d86eef1ceef3892246397
Constantine Lignos <lignos@seas.upenn.edu>
2012-09-26 09:56:35 -0400

    Fixes typo in copyright notice.

--- SubSimNLDemo/Demo.cs
+++ SubSimNLDemo/Demo.cs
@@ -2,20 +2,20 @@
  *  Demo.cs - Launches the NL SubSim for standalone testing.
  *  Copyright (C) 2011 Constantine Lignos
  *
- *  This file is part of SumSimNL.
+ *  This file is part of SubSimNL.
  *
- *  SumSimNL is free software: you can redistribute it and/or modify
+ *  SubSimNL is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  SumSimNL is distributed in the hope that it will be useful,
+ *  SubSimNL is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with SumSimNL.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with SubSimNL.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 using System;
@@ -53,7 +53,7 @@ namespace SubSimNLDemo
 			    brain.useCamera = false;
 			    brain.useRemoteVoice = false;
 			    brain.voicePort = "";
-                brain.configFileName = @"C:\Users\Administrator\Documents\Visual Studio 2008\Projects\SubSimNL_2.0\SubSimNLDemo\BrainInterface.exe.config";
+                brain.configFileName = @"C:\Users\Administrator\checkout\SubSimNL\SubSimNLDemo\BrainInterface.exe.config";
                 brain.Init(IntPtr.Zero, ""); 
                 robot = brain.Robot;
             }
--- SubSimNLProcessor/NLInputForm.cs
+++ SubSimNLProcessor/NLInputForm.cs
@@ -2,20 +2,20 @@
  *  NLInputForm.cs - Form for accepting natural language input.
  *  Copyright (C) 2011 Constantine Lignos
  *
- *  This file is part of SumSimNL.
+ *  This file is part of SubSimNL.
  *
- *  SumSimNL is free software: you can redistribute it and/or modify
+ *  SubSimNL is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  SumSimNL is distributed in the hope that it will be useful,
+ *  SubSimNL is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with SumSimNL.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with SubSimNL.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 using System;
